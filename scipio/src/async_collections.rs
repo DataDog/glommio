3999046e7d730244958c6edf69fef75014924237
Glauber Costa <glauber@datadoghq.com>
2020-09-06 12:53:04 -0400

    add examples to the AsyncDeque public functions
    AFAICT right now all public functions are documented.

--- scipio/src/async_collections.rs
+++ scipio/src/async_collections.rs
@@ -20,7 +20,16 @@ pub struct AsyncDeque<T> {
 }
 
 impl<T> AsyncDeque<T> {
-    /// Creates a new queue
+    /// Creates a new Async Deque
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::AsyncDeque;
+    ///
+    /// let _ : AsyncDeque<usize> = AsyncDeque::new();
+    ///
+    /// ```
     pub fn new() -> Self {
         AsyncDeque {
             deque: RefCell::new(VecDeque::new()),
@@ -30,11 +39,29 @@ impl<T> AsyncDeque<T> {
 
     /// Closes the current queue. All waiters will return Err(), and
     /// no new waiters are accepted.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::AsyncDeque;
+    ///
+    /// let ad : AsyncDeque<usize> = AsyncDeque::new();
+    /// ad.close();
+    /// ```
     pub fn close(&self) {
         self.sem.close();
     }
 
     /// Creates a new queue with the specified capacity
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::AsyncDeque;
+    ///
+    /// let _ : AsyncDeque<usize> = AsyncDeque::with_capacity(10);
+    ///
+    /// ```
     pub fn with_capacity(capacity: usize) -> Self {
         AsyncDeque {
             deque: RefCell::new(VecDeque::with_capacity(capacity)),
@@ -43,17 +70,46 @@ impl<T> AsyncDeque<T> {
     }
 
     /// returns the amount of elements currently in the queue
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::AsyncDeque;
+    ///
+    /// let ad : AsyncDeque<usize> = AsyncDeque::with_capacity(10);
+    /// ad.push_back(1);
+    /// assert_eq!(ad.len(), 1);
+    /// ```
     pub fn len(&self) -> usize {
         self.deque.borrow().len()
     }
 
     /// pushes an element to the front of the queue
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::AsyncDeque;
+    ///
+    /// let ad : AsyncDeque<usize> = AsyncDeque::with_capacity(10);
+    /// ad.push_front(1);
+    /// assert_eq!(ad.len(), 1);
+    /// ```
     pub fn push_front(&self, el: T) {
         self.deque.borrow_mut().push_front(el);
         self.sem.signal(1);
     }
 
     /// pushes an element to the back of the queue
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::AsyncDeque;
+    ///
+    /// let ad : AsyncDeque<usize> = AsyncDeque::with_capacity(10);
+    /// ad.push_back(1);
+    /// assert_eq!(ad.len(), 1);
+    /// ```
     pub fn push_back(&self, el: T) {
         self.deque.borrow_mut().push_front(el);
         self.sem.signal(1);
@@ -64,6 +120,22 @@ impl<T> AsyncDeque<T> {
     /// If the queue is empty, this blocks until an element can be pop'd.
     ///
     /// This method returns Ok(T) unless the collection is closed.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::{LocalExecutor, AsyncDeque};
+    ///
+    /// let ad : AsyncDeque<usize> = AsyncDeque::with_capacity(10);
+    ///
+    /// let ex = LocalExecutor::new(None).unwrap();
+    /// ex.run(async move {
+    ///     ad.push_front(1);
+    ///     ad.push_front(2);
+    ///     let res = ad.pop_back().await.unwrap();
+    ///     assert_eq!(res, 1);
+    /// });
+    /// ```
     pub async fn pop_back(&self) -> Result<T> {
         self.sem.acquire(1).await?;
         Ok(self.deque.borrow_mut().pop_back().unwrap())
@@ -74,6 +146,22 @@ impl<T> AsyncDeque<T> {
     /// If the queue is empty, this blocks until an element can be pop'd.
     ///
     /// This method returns Ok(T) unless the collection is closed.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use scipio::{LocalExecutor, AsyncDeque};
+    ///
+    /// let ad : AsyncDeque<usize> = AsyncDeque::with_capacity(10);
+    ///
+    /// let ex = LocalExecutor::new(None).unwrap();
+    /// ex.run(async move {
+    ///     ad.push_front(1);
+    ///     ad.push_front(2);
+    ///     let res = ad.pop_front().await.unwrap();
+    ///     assert_eq!(res, 2);
+    /// });
+    /// ```
     pub async fn pop_front(&self) -> Result<T> {
         self.sem.acquire(1).await?;
         Ok(self.deque.borrow_mut().pop_front().unwrap())
