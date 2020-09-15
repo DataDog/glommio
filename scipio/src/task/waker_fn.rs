cc91d233498fa886f70f1aedfe7cbfa557735db8
Glauber Costa <glauber@datadoghq.com>
2020-09-15 11:48:01 -0400

    waker_fn: change the implementation to use an Rc.
    It was inherited as an Arc from async-task, but it seems to me we can
    safely make the switch. All tests pass after this.

--- scipio/src/task/waker_fn.rs
+++ scipio/src/task/waker_fn.rs
@@ -3,15 +3,15 @@
 //
 // This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2020 Datadog, Inc.
 //
-use alloc::sync::Arc;
 use core::mem::{self, ManuallyDrop};
 use core::task::{RawWaker, RawWakerVTable, Waker};
+use std::rc::Rc;
 
 /// Creates a waker from a wake function.
 ///
 /// The function gets called every time the waker is woken.
 pub fn waker_fn<F: Fn() + Send + Sync + 'static>(f: F) -> Waker {
-    let raw = Arc::into_raw(Arc::new(f)) as *const ();
+    let raw = Rc::into_raw(Rc::new(f)) as *const ();
     let vtable = &Helper::<F>::VTABLE;
     unsafe { Waker::from_raw(RawWaker::new(raw, vtable)) }
 }
@@ -27,22 +27,22 @@ impl<F: Fn() + Send + Sync + 'static> Helper<F> {
     );
 
     unsafe fn clone_waker(ptr: *const ()) -> RawWaker {
-        let arc = ManuallyDrop::new(Arc::from_raw(ptr as *const F));
-        mem::forget(arc.clone());
+        let rc = ManuallyDrop::new(Rc::from_raw(ptr as *const F));
+        mem::forget(rc.clone());
         RawWaker::new(ptr, &Self::VTABLE)
     }
 
     unsafe fn wake(ptr: *const ()) {
-        let arc = Arc::from_raw(ptr as *const F);
-        (arc)();
+        let rc = Rc::from_raw(ptr as *const F);
+        (rc)();
     }
 
     unsafe fn wake_by_ref(ptr: *const ()) {
-        let arc = ManuallyDrop::new(Arc::from_raw(ptr as *const F));
-        (arc)();
+        let rc = ManuallyDrop::new(Rc::from_raw(ptr as *const F));
+        (rc)();
     }
 
     unsafe fn drop_waker(ptr: *const ()) {
-        drop(Arc::from_raw(ptr as *const F));
+        drop(Rc::from_raw(ptr as *const F));
     }
 }
