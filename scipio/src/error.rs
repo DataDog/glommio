DataDog/glommio

commit 9c2b203568bcd4855ef876b42119de8e6bdfa33d

Cleanup ErrorEnhancer

scipio/src/error.rs
@@ -7,7 +7,7 @@ use std::fmt;
 use std::os::unix::io::RawFd;
 use std::path::PathBuf;
 
-/// Augments an io::Error with more information about what was happening
+/// Augments an `io::Error` with more information about what was happening
 /// and to which file when the error ocurred.
 pub(crate) struct ErrorEnhancer {
     pub(crate) inner: std::io::Error,
@@ -25,11 +25,11 @@ impl fmt::Debug for ErrorEnhancer {
 impl fmt::Display for ErrorEnhancer {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}, op: {}", self.inner, self.op)?;
-        if let Some(path) = &self.path.as_ref().and_then(|x| x.to_str()) {
-            write!(f, " path {}", path)?;
+        if let Some(path) = &self.path {
+            write!(f, " path {}", path.display())?;
         }
 
-        if let Some(fd) = &self.fd {
+        if let Some(fd) = self.fd {
             write!(f, " with fd {}", fd)?;
         }
         Ok(())
@@ -38,6 +38,6 @@ impl fmt::Display for ErrorEnhancer {
 
 impl From<ErrorEnhancer> for std::io::Error {
     fn from(err: ErrorEnhancer) -> std::io::Error {
-        std::io::Error::new(err.inner.kind(), format!("{}", err.inner))
+        err.inner
     }
 }

commit a3c44db377bf78a736635f682d9ed2b1f2f12d24

make sure that the enhanced io::Error correctly implements into_inner()
The Enhanced Error mechanism expects err.into_inner() to work and the extended
message to be present in the inner field of io::Error

Commit 9c2b203568bcd4855ef876b42119de8e6bdfa33d broke this, by simply returning
inner without enhancing it.

scipio/src/error.rs
@@ -38,6 +38,6 @@ impl fmt::Display for ErrorEnhancer {
 
 impl From<ErrorEnhancer> for std::io::Error {
     fn from(err: ErrorEnhancer) -> std::io::Error {
-        err.inner
+        std::io::Error::new(err.inner.kind(), format!("{}", err.inner))
     }
 }