63a12a873f1ac9b5e2399093fbd66def2d160ac1
jianghua <jianghua@qiyi.com>
2021-02-06 01:10:48 +0800

    support partial mesh

--- glommio/src/channels/channel_mesh.rs
+++ glommio/src/channels/channel_mesh.rs
@@ -6,7 +6,7 @@
 use std::cell::Cell;
 use std::fmt::{self, Debug, Formatter};
 use std::io::{Error, ErrorKind};
-use std::iter::repeat_with;
+
 use std::sync::{Arc, RwLock};
 
 use crate::channels::shared_channel::{self, *};
@@ -15,75 +15,96 @@ use crate::{GlommioError, Local, Result, Task};
 #[derive(Debug)]
 struct Senders<T: Send + Copy> {
     peer_id: usize,
+    producer_id: Option<usize>,
     senders: Vec<Option<ConnectedSender<T>>>,
 }
 
 impl<T: Send + Copy> Senders<T> {
-    pub fn id(&self) -> usize {
+    /// Index of the local executor in the list of all peers
+    pub fn peer_id(&self) -> usize {
         self.peer_id
     }
 
-    pub fn nr_peers(&self) -> usize {
+    /// Index of the local executor in the list of producers
+    pub fn producer_id(&self) -> Option<usize> {
+        self.producer_id
+    }
+
+    /// Number of peers to which messages can be sent.
+    pub fn nr_consumers(&self) -> usize {
         self.senders.len()
     }
 
-    pub async fn send_to(&self, peer_id: usize, msg: T) -> Result<(), T> {
-        if peer_id == self.peer_id {
-            return Err(GlommioError::IoError(Error::new(
+    /// Send a message to the idx-th consumer
+    pub async fn send_to(&self, idx: usize, msg: T) -> Result<(), T> {
+        match self.senders[idx].as_ref() {
+            Some(consumer) => consumer.send(msg).await,
+            None => Err(GlommioError::IoError(Error::new(
                 ErrorKind::InvalidInput,
                 "Local message should not be sent via channel mesh",
-            )));
+            ))),
         }
-        self.senders[peer_id].as_ref().unwrap().send(msg).await
     }
 
-    pub fn try_send_to(&self, peer_id: usize, msg: T) -> Result<(), T> {
-        if peer_id == self.peer_id {
-            return Err(GlommioError::IoError(Error::new(
+    /// Try sending a message to the idx-th consumer
+    pub fn try_send_to(&self, idx: usize, msg: T) -> Result<(), T> {
+        match self.senders[idx].as_ref() {
+            Some(consumer) => consumer.try_send(msg),
+            None => Err(GlommioError::IoError(Error::new(
                 ErrorKind::InvalidInput,
                 "Local message should not be sent via channel mesh",
-            )));
+            ))),
         }
-        self.senders[peer_id].as_ref().unwrap().try_send(msg)
     }
 }
 
 #[derive(Debug)]
 struct Receivers<T: Send + Copy> {
     peer_id: usize,
+    consumer_id: Option<usize>,
     receivers: Vec<Option<ConnectedReceiver<T>>>,
 }
 
 impl<T: Send + Copy> Receivers<T> {
-    pub fn id(&self) -> usize {
+    /// Index of the local executor in the list of all peers
+    pub fn peer_id(&self) -> usize {
         self.peer_id
     }
 
-    pub fn nr_peers(&self) -> usize {
+    /// Index of the local executor in the list of consumers.
+    pub fn consumer_id(&self) -> Option<usize> {
+        self.consumer_id
+    }
+
+    /// Number of peers from which message can be received.
+    pub fn nr_producers(&self) -> usize {
         self.receivers.len()
     }
 
-    pub async fn recv_from(&self, peer_id: usize) -> Result<Option<T>, ()> {
-        if peer_id == self.peer_id {
-            return Err(GlommioError::IoError(Error::new(
+    /// Receive a message from the idx-th producer
+    pub async fn recv_from(&self, idx: usize) -> Result<Option<T>, ()> {
+        match self.receivers[idx].as_ref() {
+            Some(producer) => Ok(producer.recv().await),
+            None => Err(GlommioError::IoError(Error::new(
                 ErrorKind::InvalidInput,
                 "Local message should not be received from channel mesh",
-            )));
+            ))),
         }
-        Ok(self.receivers[peer_id].as_ref().unwrap().recv().await)
     }
 }
 
-struct Notifier {
+struct Peer {
     executor_id: usize,
-    sender: Option<SharedSender<bool>>,
+    notifier: Option<SharedSender<bool>>,
+    role: Role,
 }
 
-impl Notifier {
-    fn new(sender: Option<SharedSender<bool>>) -> Self {
+impl Peer {
+    fn new(sender: Option<SharedSender<bool>>, role: Role) -> Self {
         Self {
             executor_id: Local::id(),
-            sender,
+            notifier: sender,
+            role,
         }
     }
 }
@@ -95,16 +116,66 @@ type SharedChannel<T> = (
 
 type SharedChannels<T> = Vec<Vec<SharedChannel<T>>>;
 
-#[derive(Clone)]
-pub struct MeshBuilder<T: Send + Copy> {
-    nr_peers: usize,
-    notifiers: Arc<RwLock<Vec<Notifier>>>,
-    channels: Arc<SharedChannels<T>>,
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Role {
+    Producer,
+    Consumer,
+    Both,
 }
 
-unsafe impl<T: Send + Copy> Send for MeshBuilder<T> {}
+impl Role {
+    fn is_producer(&self) -> bool {
+        Self::Consumer.ne(self)
+    }
 
-impl<T: Send + Copy> Debug for MeshBuilder<T> {
+    fn is_consumer(&self) -> bool {
+        Self::Producer.ne(self)
+    }
+}
+
+pub trait MeshAdapter {
+    fn connect(&self, from: &Role, to: &Role) -> bool;
+    fn is_full(&self) -> bool;
+}
+
+#[derive(Clone)]
+struct Full;
+
+impl MeshAdapter for Full {
+    fn connect(&self, _: &Role, _: &Role) -> bool {
+        true
+    }
+
+    fn is_full(&self) -> bool {
+        true
+    }
+}
+
+#[derive(Clone)]
+struct Partial;
+
+impl MeshAdapter for Partial {
+    fn connect(&self, from: &Role, to: &Role) -> bool {
+        matches!((from, to), (Role::Producer, Role::Consumer))
+    }
+
+    fn is_full(&self) -> bool {
+        false
+    }
+}
+
+#[derive(Clone)]
+pub struct MeshBuilder<T: Send + Copy, A: MeshAdapter> {
+    nr_peers: usize,
+    channel_size: usize,
+    peers: Arc<RwLock<Vec<Peer>>>,
+    channels: Arc<SharedChannels<T>>,
+    adapter: A,
+}
+
+unsafe impl<T: Send + Copy, A: MeshAdapter> Send for MeshBuilder<T, A> {}
+
+impl<T: Send + Copy, A: MeshAdapter> Debug for MeshBuilder<T, A> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         f.write_fmt(format_args!(
             "MeshBuilder {{ nr_peers: {} }}",
@@ -113,63 +184,95 @@ impl<T: Send + Copy> Debug for MeshBuilder<T> {
     }
 }
 
-impl<T: 'static + Send + Copy> MeshBuilder<T> {
-    pub fn new(nr_peers: usize, channel_size: usize) -> Self {
+impl<T: 'static + Send + Copy> MeshBuilder<T, Full> {
+    /// Create a full mesh builder.
+    pub fn full(nr_peers: usize, channel_size: usize) -> Self {
+        Self::new(nr_peers, channel_size, Full)
+    }
+
+    /// Join a full mesh
+    pub async fn join(self) -> Result<(Senders<T>, Receivers<T>), ()> {
+        self.join_with(Role::Both).await
+    }
+}
+
+impl<T: 'static + Send + Copy> MeshBuilder<T, Partial> {
+    /// Create a partial mesh builder.
+    pub fn partial(nr_peers: usize, channel_size: usize) -> Self {
+        Self::new(nr_peers, channel_size, Partial)
+    }
+
+    /// Join a partial mesh
+    pub async fn join(self, role: Role) -> Result<(Senders<T>, Receivers<T>), ()> {
+        self.join_with(role).await
+    }
+}
+
+impl<T: 'static + Send + Copy, A: MeshAdapter> MeshBuilder<T, A> {
+    fn new(nr_peers: usize, channel_size: usize, adapter: A) -> Self {
         MeshBuilder {
             nr_peers,
-            notifiers: Arc::new(RwLock::new(Vec::new())),
-            channels: Arc::new(Self::build_channels(nr_peers, channel_size)),
+            channel_size,
+            peers: Arc::new(RwLock::new(Vec::new())),
+            channels: Arc::new(Self::placeholder(nr_peers)),
+            adapter,
         }
     }
 
-    fn build_channels(nr_peers: usize, channel_size: usize) -> SharedChannels<T> {
-        let mut channels: Vec<_> = repeat_with(Vec::new).take(nr_peers).collect();
-        for (i, vec) in channels.iter_mut().enumerate() {
-            vec.resize_with(i, || Self::build_channel(channel_size));
-            vec.resize_with(i + 1, || (Cell::new(None), Cell::new(None)));
-            vec.resize_with(nr_peers, || Self::build_channel(channel_size));
-        }
-        channels
+    fn placeholder(nr_peers: usize) -> SharedChannels<T> {
+        (0..nr_peers)
+            .map(|_| {
+                (0..nr_peers)
+                    .map(|_| (Cell::new(None), Cell::new(None)))
+                    .collect()
+            })
+            .collect()
     }
 
-    fn build_channel(channel_size: usize) -> SharedChannel<T> {
-        let (sender, receiver) = shared_channel::new_bounded(channel_size);
-        (Cell::new(Some(sender)), Cell::new(Some(receiver)))
-    }
+    fn register(&self, role: Role) -> Result<RegisterResult<bool>, ()> {
+        let mut peers = self.peers.write().unwrap();
 
-    fn register(&self) -> Result<RegisterResult<bool>, ()> {
-        let mut notifiers = self.notifiers.write().unwrap();
-
-        if notifiers.len() == self.nr_peers {
+        if peers.len() == self.nr_peers {
             return Err(GlommioError::IoError(Error::new(
                 ErrorKind::Other,
                 "The channel mesh is full.",
             )));
         }
 
-        let index = notifiers
+        let index = peers
             .binary_search_by(|n| n.executor_id.cmp(&Local::id()))
             .expect_err("Should not join a mesh more than once.");
 
-        if notifiers.len() == self.nr_peers - 1 {
-            notifiers.insert(index, Notifier::new(None));
+        if peers.len() == self.nr_peers - 1 {
+            peers.insert(index, Peer::new(None, role));
 
-            let peers: Vec<_> = notifiers
+            for (idx_from, from) in peers.iter().enumerate() {
+                for (idx_to, to) in peers.iter().enumerate() {
+                    let channel = &self.channels[idx_from][idx_to];
+                    if idx_from != idx_to && self.adapter.connect(&from.role, &to.role) {
+                        let (sender, receiver) = shared_channel::new_bounded(self.channel_size);
+                        channel.0.set(Some(sender));
+                        channel.1.set(Some(receiver));
+                    }
+                }
+            }
+
+            let peers: Vec<_> = peers
                 .iter_mut()
-                .map(|notifier| notifier.sender.take())
+                .map(|notifier| notifier.notifier.take())
                 .flatten()
                 .collect();
 
             Ok(RegisterResult::NotificationSenders(peers))
         } else {
             let (sender, receiver) = shared_channel::new_bounded(1);
-            notifiers.insert(index, Notifier::new(Some(sender)));
+            peers.insert(index, Peer::new(Some(sender), role));
             Ok(RegisterResult::NotificationReceiver(receiver))
         }
     }
 
-    async fn join(self) -> Result<(Senders<T>, Receivers<T>), ()> {
-        match Self::register(&self)? {
+    async fn join_with(self, role: Role) -> Result<(Senders<T>, Receivers<T>), ()> {
+        match Self::register(&self, role)? {
             RegisterResult::NotificationReceiver(receiver) => {
                 receiver.connect().await.recv().await.unwrap();
             }
@@ -180,36 +283,71 @@ impl<T: 'static + Send + Copy> MeshBuilder<T> {
             }
         }
 
-        let peer_id = {
-            let notifiers = self.notifiers.read().unwrap();
-
-            notifiers
+        let (peer_id, role_id) = {
+            let peers = self.peers.read().unwrap();
+            let peer_id = peers
                 .binary_search_by(|n| n.executor_id.cmp(&Local::id()))
-                .unwrap()
+                .unwrap();
+            let role_id = peers
+                .iter()
+                .take(peer_id)
+                .filter(|r| r.role.eq(&role))
+                .count();
+            (peer_id, role_id)
+        };
+
+        let producer_id = if role.is_producer() {
+            Some(role_id)
+        } else {
+            None
+        };
+
+        let consumer_id = if role.is_consumer() {
+            Some(role_id)
+        } else {
+            None
         };
 
         let mut senders = Vec::with_capacity(self.nr_peers);
         let mut receivers = Vec::with_capacity(self.nr_peers);
 
         for i in 0..self.nr_peers {
-            if peer_id == i {
-                senders.push(None);
-                receivers.push(None);
-            } else {
-                let sender = self.channels[peer_id][i].0.take().unwrap();
-                let sender = Task::<_>::local(sender.connect()).detach();
+            let sender = self.channels[peer_id][i].0.take();
+            let receiver = self.channels[i][peer_id].1.take();
 
-                let receiver = self.channels[i][peer_id].1.take().unwrap();
-                let receiver = Task::<_>::local(receiver.connect()).detach();
+            let sender = sender.map(|sender| Task::<_>::local(sender.connect()).detach());
+            let receiver = receiver.map(|receiver| Task::<_>::local(receiver.connect()).detach());
 
-                senders.push(Some(sender.await.unwrap()));
-                receivers.push(Some(receiver.await.unwrap()));
+            match sender {
+                None => {
+                    if self.adapter.is_full() {
+                        senders.push(None)
+                    }
+                }
+                Some(sender) => senders.push(Some(sender.await.unwrap())),
+            }
+
+            match receiver {
+                None => {
+                    if self.adapter.is_full() {
+                        receivers.push(None)
+                    }
+                }
+                Some(receiver) => receivers.push(Some(receiver.await.unwrap())),
             }
         }
 
         Ok((
-            Senders { peer_id, senders },
-            Receivers { peer_id, receivers },
+            Senders {
+                peer_id,
+                producer_id,
+                senders,
+            },
+            Receivers {
+                peer_id,
+                consumer_id,
+                receivers,
+            },
         ))
     }
 }
@@ -239,19 +377,19 @@ mod tests {
     fn test_peer_id_invariance() {
         let nr_peers = 10;
         let channel_size = 100;
-        let builder1 = MeshBuilder::<i32>::new(nr_peers, channel_size);
-        let builder2 = MeshBuilder::<usize>::new(nr_peers, channel_size);
+        let builder1 = MeshBuilder::<i32, _>::full(nr_peers, channel_size);
+        let builder2 = MeshBuilder::<usize, _>::full(nr_peers, channel_size);
 
         let executors = (0..nr_peers).map(|_| {
             LocalExecutorBuilder::new().spawn(enclose!((builder1, builder2) move || async move {
                 let (sender1, receiver1) = builder1.join().await.unwrap();
-                assert_eq!(sender1.id(), receiver1.id());
+                assert_eq!(sender1.peer_id(), receiver1.peer_id());
 
                 let (sender2, receiver2) = builder2.join().await.unwrap();
-                assert_eq!(sender2.id(), receiver2.id());
+                assert_eq!(sender2.peer_id(), receiver2.peer_id());
 
-                assert_eq!(sender1.id(), sender2.id());
-                assert_eq!(receiver1.id(), receiver2.id());
+                assert_eq!(sender1.peer_id(), sender2.peer_id());
+                assert_eq!(receiver1.peer_id(), receiver2.peer_id());
             }))
         });
 
@@ -263,7 +401,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_join_more_than_once() {
-        let mesh_builder = MeshBuilder::<u32>::new(2, 1);
+        let mesh_builder = MeshBuilder::<u32, _>::full(2, 1);
 
         LocalExecutor::default().run(enclose!((mesh_builder) async move {
             future::join(mesh_builder.clone().join(), mesh_builder.join()).await;
@@ -277,28 +415,72 @@ mod tests {
     }
 
     fn do_test_channel_mesh(nr_peers: usize, channel_size: usize, nr_executors: usize) {
-        let mesh_builder = MeshBuilder::new(nr_peers, channel_size);
+        let mesh_builder = MeshBuilder::full(nr_peers, channel_size);
 
         let executors = (0..nr_executors).map(|_| {
             LocalExecutorBuilder::new().spawn(enclose!((mesh_builder) move || async move {
                 let (sender, receiver) = mesh_builder.join().await.unwrap();
+                assert_eq!(nr_peers, sender.nr_consumers());
+                assert_eq!(sender.peer_id, sender.producer_id.unwrap());
+                assert_eq!(nr_peers, receiver.nr_producers());
+                assert_eq!(receiver.peer_id, receiver.consumer_id.unwrap());
+
                 Local::local(async move {
-                    for peer in 0..sender.nr_peers() {
-                        if peer != sender.id() {
-                            sender.send_to(peer, (sender.id(), peer)).await.unwrap();
+                    for peer in 0..sender.nr_consumers() {
+                        if peer != sender.peer_id() {
+                            sender.send_to(peer, (sender.peer_id(), peer)).await.unwrap();
                         }
                     }
                 }).detach();
 
-                for peer in 0..receiver.nr_peers() {
-                    if peer != receiver.id() {
-                        assert_eq!((peer, receiver.id()), receiver.recv_from(peer).await.unwrap().unwrap());
+                for peer in 0..receiver.nr_producers() {
+                    if peer != receiver.peer_id() {
+                        assert_eq!((peer, receiver.peer_id()), receiver.recv_from(peer).await.unwrap().unwrap());
                     }
                 }
             }))
         });
 
         for ex in executors.collect::<Vec<_>>() {
+            ex.unwrap().join().unwrap();
+        }
+    }
+
+    #[test]
+    fn test_partial_mesh() {
+        let nr_producers = 2;
+        let nr_consumers = 3;
+        let mesh_builder = MeshBuilder::new(5, 100, Partial);
+
+        let producers = (0..nr_producers).map(|i| {
+            LocalExecutorBuilder::new().spawn(enclose!((mesh_builder) move || async move {
+                let (sender, receiver) = mesh_builder.join(Role::Producer).await.unwrap();
+                assert_eq!(nr_consumers, sender.nr_consumers());
+                assert_eq!(Some(i), sender.producer_id());
+                assert_eq!(0, receiver.nr_producers());
+                assert_eq!(None, receiver.consumer_id());
+
+                for consumer_id in 0..sender.nr_consumers() {
+                    sender.send_to(consumer_id, (sender.producer_id().unwrap(), consumer_id)).await.unwrap();
+                }
+            }))
+        });
+
+        let consumers = (0..nr_consumers).map(|i| {
+            LocalExecutorBuilder::new().spawn(enclose!((mesh_builder) move || async move {
+                let (sender, receiver) = mesh_builder.join(Role::Consumer).await.unwrap();
+                assert_eq!(0, sender.nr_consumers());
+                assert_eq!(None, sender.producer_id());
+                assert_eq!(nr_producers, receiver.nr_producers());
+                assert_eq!(Some(i), receiver.consumer_id());
+
+                for producer_id in 0..receiver.nr_producers() {
+                    assert_eq!((producer_id, receiver.consumer_id().unwrap()), receiver.recv_from(producer_id).await.unwrap().unwrap());
+                }
+            }))
+        });
+
+        for ex in producers.chain(consumers).collect::<Vec<_>>() {
             ex.unwrap().join().unwrap();
         }
     }
