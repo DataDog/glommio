0592e492474c842cb00b4867c15c2e8bde3d169c
hippolyte.barraud <hippolyte.barraud@datadoghq.com>
2021-05-05 20:16:46 +0200

    make enhanced_try use only public fields/methods

--- glommio/src/io/mod.rs
+++ glommio/src/io/mod.rs
@@ -100,14 +100,7 @@ macro_rules! enhanced_try {
             }
         }
     }};
-    ($expr:expr, $op:expr, $obj:expr) => {{
-        enhanced_try!(
-            $expr,
-            $op,
-            $obj.path.as_ref().and_then(|x| Some(x.as_path())),
-            Some($obj.as_raw_fd())
-        )
-    }};
+    ($expr:expr, $op:expr, $obj:expr) => {{ enhanced_try!($expr, $op, $obj.path(), Some($obj.as_raw_fd())) }};
 }
 
 mod buffered_file;
