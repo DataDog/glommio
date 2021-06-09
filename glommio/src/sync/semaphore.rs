DataDog/glommio

commit 5dc9048bfffe3bac6d5997e3525df2e688008c6c

improve readability

glommio/src/sync/semaphore.rs
@@ -547,12 +547,12 @@ impl Semaphore {
     ///  If semaphore is closed
     /// `Err(GlommioError::Closed(ResourceType::Semaphore { .. }))` will be
     /// returned.  If semaphore does not have sufficient amount of units
-    ///  `
+    ///  ```
     ///  Err(GlommioError::WouldBlock(ResourceType::Semaphore {
     ///      requested: u64,
     ///      available: u64
     ///  }))
-    ///  `
+    ///  ```
     ///  will be returned.
     ///
     /// # Examples
@@ -598,12 +598,12 @@ impl Semaphore {
     ///  If semaphore is closed
     /// `Err(GlommioError::Closed(ResourceType::Semaphore { .. }))` will be
     /// returned.  If semaphore does not have sufficient amount of units
-    ///  `
+    ///  ```
     ///  Err(GlommioError::WouldBlock(ResourceType::Semaphore {
     ///      requested: u64,
     ///      available: u64
     ///  }))
-    ///  `
+    ///  ```
     ///  will be returned.
     ///
     /// # Examples

commit efa863f974175875ad1d5c251a077c4cb7eb057e

Fix doctest, broken by 5dc9048bfffe3bac6d5997e3525df2e688008c6c

glommio/src/sync/semaphore.rs
@@ -547,7 +547,7 @@ impl Semaphore {
     ///  If semaphore is closed
     /// `Err(GlommioError::Closed(ResourceType::Semaphore { .. }))` will be
     /// returned.  If semaphore does not have sufficient amount of units
-    ///  ```
+    ///  ```ignore
     ///  Err(GlommioError::WouldBlock(ResourceType::Semaphore {
     ///      requested: u64,
     ///      available: u64
@@ -598,7 +598,7 @@ impl Semaphore {
     ///  If semaphore is closed
     /// `Err(GlommioError::Closed(ResourceType::Semaphore { .. }))` will be
     /// returned.  If semaphore does not have sufficient amount of units
-    ///  ```
+    ///  ```ignore
     ///  Err(GlommioError::WouldBlock(ResourceType::Semaphore {
     ///      requested: u64,
     ///      available: u64