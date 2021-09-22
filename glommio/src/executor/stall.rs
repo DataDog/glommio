5cf0a460fc3fba4cecb8253defa5e8045ba5141b
hippolyte.barraud <hippolyte.barraud@datadoghq.com>
2021-09-22 20:09:32 +0200

    asynchronously record stack traces when a task queue goes over budget
    Knowing if a queue is stalling the reactor is nice, but finding the
    exact code location where the stall occurs remains very hard. Especially
    if a task queue hosts many concurrent fibers.
    
    To help with that, this commit introduces a stall detection mechanism
    that records stack traces of stalling tasks. It works as
    follows:
    * When a task queue is scheduled for execution, we set up a timer that
    triggers some time after the queue is expected to yield (we add a 10%
    error margin to avoid false positives). A background thread collocated
    with the local executor waits on the timer at all times.
    * When the timer fires, the thread sends a signal (SIGUSR1) to the local
    executor thread. Upon receiving the signal, the local executor records a
    complete trace of the local stack. Here we take advantage of the fact
    that by default, the kernel invokes signal handlers on top of the
    existing stack. i.e. the frames we record are those of the problematic
    user code that was meant to yield. The recorded frames are pushed on a
    non-blocking communication channel that links the signal handler and the
    local executor.
    * When a task queue yields, the local executor disarm the timer and
    checks the communication channel for potential recorded frames, if there
    are any then we can conclude that the queue stalled, so we log them.
    
    This code works in practice but has two major drawbacks:
    * The timer dance is expensive; expect a high number of syscall. Because
    of this runtime overhead, the stall detector is disabled by default. To
    opt-in, the feature `stall-detection` must be enabled at compile-time.
    * We log stalls only after the queue yield. Therefore, if there is a bug
    in your code and your queue never yields, the stall detector will never
    log the code location that's at fault (even though we probably have
    recorded the trace by then). The reason for this is that logging from a
    signal handler is illegal.

---
+++ glommio/src/executor/stall.rs
@@ -0,0 +1,283 @@
+// Unless explicitly stated otherwise all files in this repository are licensed
+// under the MIT/Apache-2.0 License, at your convenience
+//
+// This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2021 Datadog, Inc.
+//
+#![allow(dead_code)]
+
+use nix::sys;
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread::JoinHandle,
+    time::{Duration, Instant},
+};
+
+#[cfg(feature = "stall-detection")]
+thread_local! {
+    static STALL_STRACE_COLLECTOR: (
+        crossbeam::channel::Sender<backtrace::BacktraceFrame>,
+        crossbeam::channel::Receiver<backtrace::BacktraceFrame>
+    ) = crossbeam::channel::bounded(1 << 10);
+}
+
+#[cfg(feature = "stall-detection")]
+lazy_static! {
+    static ref STALL_SIG_HANDLER: signal_hook::SigId = unsafe {
+        signal_hook::low_level::register(signal_hook::consts::SIGUSR1, move || {
+            STALL_STRACE_COLLECTOR.with(|collector| {
+                if collector.0.is_full() {
+                    return;
+                }
+
+                backtrace::trace_unsynchronized(|frame| {
+                    collector
+                        .0
+                        .try_send(backtrace::BacktraceFrame::from(frame.clone()))
+                        .is_ok()
+                });
+            });
+        })
+    }
+    .unwrap();
+}
+
+#[derive(Debug)]
+pub(crate) struct StallDetector {
+    timer: Arc<sys::timerfd::TimerFd>,
+    handler: Option<JoinHandle<()>>,
+    id: usize,
+    terminated: Arc<AtomicBool>,
+}
+
+impl StallDetector {
+    #[cfg(feature = "stall-detection")]
+    pub(crate) fn new(executor_id: usize) -> std::io::Result<StallDetector> {
+        lazy_static::initialize(&STALL_SIG_HANDLER);
+
+        let timer = Arc::new(
+            sys::timerfd::TimerFd::new(
+                sys::timerfd::ClockId::CLOCK_MONOTONIC,
+                sys::timerfd::TimerFlags::empty(),
+            )
+            .map_err(std::io::Error::from)?,
+        );
+        let tid = unsafe { nix::libc::pthread_self() };
+        let terminated = Arc::new(AtomicBool::new(false));
+        let timer_handler = std::thread::spawn(enclose::enclose! { (terminated, timer) move || {
+            while timer.wait().is_ok() {
+                if terminated.load(Ordering::Acquire) {
+                    return
+                }
+                unsafe { nix::libc::pthread_kill(tid, nix::libc::SIGUSR1) };
+            }
+        }});
+
+        Ok(Self {
+            timer,
+            handler: Some(timer_handler),
+            id: executor_id,
+            terminated,
+        })
+    }
+
+    pub(crate) fn enter_task_queue(
+        &self,
+        queue_name: String,
+        start: Instant,
+        max_expected_runtime: Duration,
+    ) -> nix::Result<StallDetectorGuard<'_>> {
+        // We consider a queue to be stalling the system if it failed to yield in due
+        // time. For a given maximum expected runtime, we allow a margin of error f 10%
+        // (and an absolute minimum of 10ms) after which we record a stacktrace. i.e. a
+        // task queue has should return shortly after `need_preempt()` returns
+        // true or the stall detector triggers. For example::
+        // * If a task queue has a preempt timer of 100ms the the stall detector
+        // triggers if it doesn't yield after running for 110ms.
+        // * If a task queue has a preempt timer of 100ms the the stall detector
+        // triggers if it doesn't yield after running for 15ms.
+
+        let error_margin =
+            Duration::from_millis((max_expected_runtime.as_millis() as f64 * 0.1) as u64)
+                .max(Duration::from_millis(10));
+        StallDetectorGuard::new(
+            self,
+            queue_name,
+            start,
+            max_expected_runtime.saturating_add(error_margin),
+        )
+    }
+
+    fn arm(&self, threshold: Duration) -> nix::Result<()> {
+        self.timer.set(
+            sys::timerfd::Expiration::OneShot(sys::time::TimeSpec::from(threshold)),
+            sys::timerfd::TimerSetTimeFlags::empty(),
+        )
+    }
+
+    fn disarm(&self) -> nix::Result<()> {
+        self.timer.unset()
+    }
+}
+
+impl Drop for StallDetector {
+    fn drop(&mut self) {
+        let timer_handler = self.handler.take().unwrap();
+        self.terminated.store(true, Ordering::Release);
+
+        self.timer
+            .set(
+                sys::timerfd::Expiration::Interval(sys::time::TimeSpec::from(
+                    Duration::from_millis(1),
+                )),
+                sys::timerfd::TimerSetTimeFlags::empty(),
+            )
+            .expect("failed wake the timer for termination");
+
+        let _ = timer_handler.join();
+    }
+}
+
+pub(crate) struct StallDetectorGuard<'detector> {
+    detector: &'detector StallDetector,
+    queue_name: String,
+    start: Instant,
+    threshold: Duration,
+}
+
+impl<'detector> StallDetectorGuard<'detector> {
+    fn new(
+        detector: &'detector StallDetector,
+        queue_name: String,
+        start: Instant,
+        threshold: Duration,
+    ) -> nix::Result<Self> {
+        detector.disarm().unwrap();
+        detector.arm(threshold).unwrap();
+        Ok(Self {
+            detector,
+            queue_name,
+            start,
+            threshold,
+        })
+    }
+}
+
+#[cfg(feature = "stall-detection")]
+impl<'detector> Drop for StallDetectorGuard<'detector> {
+    fn drop(&mut self) {
+        let _ = self.detector.disarm();
+
+        let mut strace = STALL_STRACE_COLLECTOR.with(|collector| {
+            let mut frames = vec![];
+            while let Ok(frame) = collector.1.try_recv() {
+                frames.push(frame);
+            }
+            backtrace::Backtrace::from(frames)
+        });
+
+        if strace.frames().is_empty() {
+            return;
+        }
+
+        let elapsed = self.start.elapsed();
+        strace.resolve();
+        log::warn!(
+            "[stall-detector -- executor {}] task queue {} went over-budget: {:#?} (budget: \
+             {:#?}). Backtrace: {:#?}",
+            self.detector.id,
+            &self.queue_name,
+            elapsed,
+            self.threshold,
+            strace,
+        );
+    }
+}
+
+#[cfg(all(test, feature = "stall-detection"))]
+mod test {
+    use crate::{timer::sleep, LocalExecutorBuilder};
+    use logtest::Logger;
+    use std::time::{Duration, Instant};
+
+    enum ExpectedLog {
+        Expected(&'static str),
+        NotExpected(&'static str),
+    }
+
+    fn search_logs_for(logger: &mut Logger, expected: ExpectedLog) -> bool {
+        let mut found = false;
+        while let Some(event) = logger.pop() {
+            match expected {
+                ExpectedLog::Expected(str) => found |= event.args().contains(str),
+                ExpectedLog::NotExpected(str) => found |= event.args().contains(str),
+            }
+        }
+
+        match expected {
+            ExpectedLog::Expected(_) => found,
+            ExpectedLog::NotExpected(_) => !found,
+        }
+    }
+
+    #[test]
+    fn executor_stall_detector() {
+        LocalExecutorBuilder::default()
+            .preempt_timer(Duration::from_millis(50))
+            .make()
+            .unwrap()
+            .run(async {
+                let mut logger = Logger::start();
+                let now = Instant::now();
+
+                // will trigger the stall detector because we go over budget
+                while now.elapsed() < Duration::from_millis(100) {}
+
+                assert!(search_logs_for(
+                    &mut logger,
+                    ExpectedLog::NotExpected("task queue default went over-budget"),
+                ));
+
+                crate::executor().yield_task_queue_now().await; // yield the queue
+
+                assert!(search_logs_for(
+                    &mut logger,
+                    ExpectedLog::Expected("task queue default went over-budget")
+                ));
+
+                // no stall because < 50ms of un-cooperativeness
+                let now = Instant::now();
+                while now.elapsed() < Duration::from_millis(40) {}
+
+                assert!(search_logs_for(
+                    &mut logger,
+                    ExpectedLog::NotExpected("task queue default went over-budget"),
+                ));
+
+                crate::executor().yield_task_queue_now().await; // yield the queue
+
+                // no stall because a timer yields internally
+                sleep(Duration::from_millis(100)).await;
+
+                crate::executor().yield_task_queue_now().await; // yield the queue
+
+                assert!(search_logs_for(
+                    &mut logger,
+                    ExpectedLog::NotExpected("task queue default went over-budget"),
+                ));
+
+                // trigger one last time
+                let now = Instant::now();
+                while now.elapsed() < Duration::from_millis(100) {}
+
+                crate::executor().yield_task_queue_now().await; // yield the queue
+
+                assert!(search_logs_for(
+                    &mut logger,
+                    ExpectedLog::Expected("task queue default went over-budget")
+                ));
+            });
+    }
+}
