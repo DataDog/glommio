6918fa5faf375c58a02bb151c5c65d066739d178
Glauber Costa <glauber@datadoghq.com>
2021-11-25 20:44:41 -0500

    allow liburing to be vendored
    A better long term approach is to move to a full rust implementation.
    For now, allow liburing to be vendored by reading an environment
    variable with its location. In the absence of that variable, we'll
    still resort to the standard submodule update.
    
    Fixes #466 (for now)

--- glommio/build.rs
+++ glommio/build.rs
@@ -6,14 +6,21 @@ fn main() {
     let project = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
         .canonicalize()
         .unwrap();
-    let liburing = project.join("liburing");
 
-    Command::new("git")
-        .arg("submodule")
-        .arg("update")
-        .arg("--init")
-        .status()
-        .unwrap();
+    let liburing = match env::var("GLOMMIO_LIBURING_DIR") {
+        Ok(path) => PathBuf::from(path).canonicalize().unwrap(),
+        Err(_) => {
+            Command::new("git")
+                .arg("submodule")
+                .arg("update")
+                .arg("--init")
+                .status()
+                .unwrap();
+
+            project.join("liburing")
+        }
+    };
+
     // Run the configure script in OUT_DIR to get `compat.h`
     let configured_include = configure(&liburing);
 
