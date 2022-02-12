97718f3613136792455dee92e03c58102d6442bb
Hippolyte Barraud <hippolyte.barraud@datadoghq.com>
2022-02-12 07:33:48 +0100

    update the preempt benchmark
    Make the benchmark more stable by running a fixed number of times. Also,
    benchmark `yeild_if_needed` as well.

--- glommio/benches/preempt.rs
+++ glommio/benches/preempt.rs
@@ -1,21 +1,51 @@
 use glommio::prelude::*;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
-fn main() {
+fn bench_need_preempt() {
     let local_ex = LocalExecutorBuilder::new(Placement::Fixed(0))
+        .preempt_timer(Duration::from_secs(10))
         .spawn(|| async move {
             let mut runs = 0;
             let t = Instant::now();
-            while !crate::executor().need_preempt() {
+            while runs < 1_000_000 {
+                let _ = crate::executor().need_preempt();
                 runs += 1;
             }
 
             println!(
-                "cost of checking for need_preempt: {:#?}",
-                t.elapsed() / runs,
+                "cost of checking for need_preempt:\t {:#?} (ran {} times)",
+                t.elapsed() / 1_000_000,
+                1_000_000
             );
         })
         .unwrap();
 
     local_ex.join().unwrap();
+}
+
+fn bench_yield_if_needed() {
+    let local_ex = LocalExecutorBuilder::new(Placement::Fixed(0))
+        .preempt_timer(Duration::from_secs(10))
+        .spawn(|| async move {
+            let mut runs = 0;
+            let t = Instant::now();
+            while runs < 1_000_000 {
+                crate::yield_if_needed().await;
+                runs += 1;
+            }
+
+            println!(
+                "cost of calling yield_if_needed:\t {:#?} (ran {} times)",
+                t.elapsed() / 1_000_000,
+                1_000_000
+            );
+        })
+        .unwrap();
+
+    local_ex.join().unwrap();
+}
+
+fn main() {
+    bench_need_preempt();
+    bench_yield_if_needed();
 }
